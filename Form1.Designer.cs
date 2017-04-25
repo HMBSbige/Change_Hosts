8fe14b04ec59f6c0f85718835f72ac214fe05e63
Bruce Wayne <admin@bige0.com>
2017-04-26 02:01:25 +0800

    first test

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Change_Hosts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "更新Hosts";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(9, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 34);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "获取最新可用的Hosts文件\r\n加速访问Google、AutoDraw等网站";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 74);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(214, 100);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "https://raw.githubusercontent.com/racaljk/hosts/master/hosts";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 184);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(90, 16);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "清空原Hosts";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(12, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Hosts地址：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(240, 215);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Change Hosts";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Change_Hosts
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var hostsAddress = textBox1.Text;
+            var add = GetGeneralContent(hostsAddress);
+            if (add != "")
+            {
+                var str2="";
+                if (checkBox1.Checked == false)
+                {
+                    str2 = File.ReadAllText(@"C:\Windows\System32\drivers\etc\hosts", Encoding.ASCII);
+                    str2 = str2 + '\n' + add;
+                }
+                else
+                    str2 = add;
+                const string strt1 = "# Last updated: ";
+                const string strt2 = "# This work is licensed";
+
+                File.WriteAllText(@"C:\Windows\System32\drivers\etc\hosts", str2, Encoding.ASCII);
+                var hostDate= "\nHosts版本：";
+                if (hostsAddress == "https://raw.githubusercontent.com/racaljk/hosts/master/hosts")
+                    hostDate += str2.Substring(str2.IndexOf(strt1, StringComparison.Ordinal) + strt1.Length,
+                        str2.IndexOf(strt2, StringComparison.Ordinal) - (str2.IndexOf(strt1, StringComparison.Ordinal) + strt1.Length + 1));
+                else
+                    hostDate = "";
+                MessageBox.Show(@"修改成功！" + hostDate, @"提示：");
+            }
+            else
+            {
+                MessageBox.Show(@"修改失败！
+请检查是否有权限访问HOSTS或HOSTS地址是否正确", @"提示：");
+            }
+        }
+        private static string GetGeneralContent(string strUrl)
+        {
+            var strMsg = string.Empty;
+            try
+            {
+                var request = WebRequest.Create(strUrl);
+                var response = request.GetResponse();
+                var reader = new StreamReader(response.GetResponseStream(), Encoding.GetEncoding("gb2312"));
+
+                strMsg = reader.ReadToEnd();
+
+                reader.Close();
+                reader.Dispose();
+                response.Close();
+            }
+            catch
+            {
+                // ignored
+            }
+            return strMsg;
+        }
+    }
+}
